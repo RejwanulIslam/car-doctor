26d6299f2b00738e28bd3ef2c695b113a773a2ff
RejwanulIslam <174478473+RejwanulIslam@users.noreply.github.com>
2025-10-08 18:53:03 +0600

    add protected route

--- src/app/my_bookings/[id]/page.jsx
+++ src/app/my_bookings/[id]/page.jsx
@@ -1,10 +1,13 @@
 import React from 'react'
 import UpdateBookingForm from '../component/UpdateBookingForm'
+import { headers } from 'next/headers'
 
-export default async function UpdateBooking({params}) {
-  const p=await params
-  const res=await fetch(`http://localhost:3000/api/my_booking/${p.id}`)
-  const data= await res.json()
+export default async function UpdateBooking({ params }) {
+  const p = await params
+  const res = await fetch(`http://localhost:3000/api/my_booking/${p.id}`, {
+    headers: await headers()
+  })
+  const data = await res.json()
   console.log(data)
   return (
     <div><UpdateBookingForm data={data}></UpdateBookingForm></div>
--- src/app/my_bookings/page.jsx
+++ src/app/my_bookings/page.jsx
@@ -1,16 +1,16 @@
 import { headers } from "next/headers"
 import Booking from "./component/Booking"
 const fetchMyBooking = async () => {
-    const res = await fetch('http://localhost:3000/api/service',{
-    headers:headers()
+    const res = await fetch('http://localhost:3000/api/service', {
+        headers: await headers()
 
     })
-     
+
     const data = await res.json()
     return data
-   }
+}
 export default async function MyBookingsPage() {
-const data=await  fetchMyBooking()
+    const data = await fetchMyBooking()
 
     return (
         <div>
